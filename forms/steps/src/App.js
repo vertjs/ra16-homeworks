a9d953cf620a68a9935fa9e6277df03c5d72a356
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-05 21:56:34 +0300

    solution uploaded

---
+++ forms/steps/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Trainings from '../src/components/Trainings'
+import './App.css';
+
+export default function App() {
+  return (
+    <Trainings/>
+  );
+}
---
+++ forms/steps/src/components/OneTraining.js
@@ -0,0 +1,7 @@
+ export default class OneTraining {
+    constructor(id, date, distanc) {
+        this.id = id
+        this.date = date
+        this.distanc = distanc
+    }
+ }
