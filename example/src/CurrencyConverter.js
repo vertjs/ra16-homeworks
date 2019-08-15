3f29434970228c74f09f5c07df2b9700f3d468ef
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-15 10:05:06 +0300

    нет подключения к серверу

--- example/src/CurrencyConverter.js
+++ example/src/CurrencyConverter.js
@@ -1,57 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default class Calculator extends React.Component {
+export default class CurrencyConverter extends Component {
     state = {
         rubAmount: 0,
-        rate: 50
+        rate: 57,
     }
+
     componentDidMount() {
-        this.loadActualRate()
+        this.loadActualRate();
     }
 
     handleAmountChange = event => {
-        this.setState({
-            rubAmount: event.target.value
-        })
+        this.setState({ rubAmount: event.target.value });
     }
 
-    calcUSDsum() {
-        const {rubAmount, rate} = this.state
-        return (rubAmount/rate).toFixed(4)
+    calcUSDsum = () => {
+        const { rubAmount, rate } = this.state;
+        return (rubAmount / rate).toFixed(4);
     }
 
-    loadActualRate =() => {
+    loadActualRate = () => {
         fetch(process.env.REACT_APP_CURRENCY_URL)
-        .then(response => response.json())
-        .then(rates => {
-            const findUSD = rate => rate.code === "USD"
-            const rate = rates.find(findUSD).value
-            this.setState({rate})
-        })
+            .then(response => response.json())
+            .then(rates => {
+                const findUSD = rate => rate.code === 'RUR';
+                const rate = rates.find(findUSD).value;
+                this.setState({ rate })
+            })
     }
-
+    
     render() {
-        const {rubAmount, rate} = this.state
+        const { rubAmount, rate } = this.state;
         return (
             <div>
-                <h3>Конвертер валют</h3>
+                <h3>Конвертер валют:</h3>
                 <div>Текущий курс: {rate}</div>
                 <div>
-                    <span>Сумма в рублях:</span>
-                    <input 
+                    <span>Сумма в рублях: </span>
+                    <input
                         type="text"
                         placeholder="Сумма в рублях"
                         onChange={this.handleAmountChange}
-                        value={this.rubAmount}
-                    />
+                        value={rubAmount} />
                 </div>
                 <span>Сумма в долларах: {this.calcUSDsum()}</span>
                 <div>
                     <button>Загрузить курс валют</button>
                 </div>
                 <p>{process.env.REACT_APP_CARRENCY_URL}</p>
-                
             </div>
-        )
+        );
     }
 }
