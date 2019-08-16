ac9df7ae3dc77afe0a3ab11ba9d5a76462ba2973
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-16 20:11:12 +0300

    changet to functional component

--- lifecycle-http/crud/frontend/src/Crud.js
+++ lifecycle-http/crud/frontend/src/Crud.js
@@ -1,68 +1,56 @@
-import React, { Component } from 'react'
+import React,{ useState } from 'react'
 import Notes from './Notes'
+import NoteModel from './NoteModel'
 import nanoid from 'nanoid';
 
-export default class Crud extends Component {
-    state = {
-        notes: [],
-        value: ''
-    }
+export default function Crud() {
+  const [notes, setNotes] = useState([])
+  const [form, setForm] = useState({content: ''})
 
-    handleSubmit = (e) => {
+  const handleSubmit = (e) => {
       e.preventDefault();
-      const id = Math.random().toString().slice(2, 4);
-      const newNote = { id, content: this.state.value };
-      this.setState({notes: [...this.state.notes, newNote] })
-      console.log(this.state.notes)
-      console.log(this.state.value)
-      
-    }
+      const newNote = new NoteModel(nanoid(), form.content)
+      console.log(newNote) 
+      setNotes(prevNotes => [...prevNotes, newNote])
+      setForm({content: ''})
+      console.log(notes) 
+  }
 
-    handleChange = (e) => {
-      this.setState({ ...this.state, value: e.target.value });
-      console.log(this.state.value)
-    }
+  const handleChange = (e) => {
+    const {value} = e.target
+    setForm(prev => ({...prev, content: value}))
+    console.log(form.content)
+  }
 
-    componentDidMount() {
-      this.loadActualNotes();
-    }
-
-    componentWillUnmount() {
-      this.loadActualNotes();
-    }
-
-    loadActualNotes = () => {
-      console.log(process.env.REACT_APP_API_URL);
-      console.log(this.state.notes)
-      fetch(`${process.env.REACT_APP_API_URL}`)
-      .then(response => response.json())
-      .then(arr => arr.map(el => this.setState({notes: [...this.state.notes, el] })))
-      //.then(rates => this.setState({ notes: rates })) 
-      console.log(this.state.notes)
-    //  
-    }
-
-    render() {
-        return (
-            <div>
-            <form onSubmit={this.handleSubmit} className="form">
-              <textarea
-                rows="3"
-                cols="50"
-                placeholder="Ввести текст"
-                onChange={this.handleChange}
-                value={this.state.value}
-                key={nanoid()}
-              />
-              <div className="btn">
-                <button>Добавить заметку</button>
-                <button type="button" onClick={this.loadActualNotes}>Загрузить заметки</button>
-              </div>
-            </form>
-            <div className="App">
-             { /*<Notes notes={this.state.notes} />*/}
-            </div>
+  const loadActualNotes = () => {
+    console.log(process.env.REACT_APP_API_URL);
+    console.log(notes)
+    fetch(`${process.env.REACT_APP_API_URL}`)
+    .then(response => response.json())
+    .then(arr => arr.map(el => setNotes(prevNotes => [...prevNotes, el])))
+    
+    console.log(notes)
+  }
+    return (
+      <div>
+        <form onSubmit={handleSubmit} className="form">
+          <textarea
+            rows="3"
+            cols="50"
+            placeholder="Ввести текст"
+            onChange={handleChange}
+            value={form.content}
+            key={nanoid()}
+          />
+          <div className="btn">
+            <button>Добавить заметку</button>
+            <button type="button" onClick={loadActualNotes}>Загрузить заметки</button>
           </div>
-        )
-    }
+        </form>
+        <div className="App">
+          { /*<Notes notes={this.state.notes} />*/}
+        </div>
+    </div>
+    )
+  
 }
---
+++ lifecycle-http/crud/frontend/src/NoteModel.js
@@ -0,0 +1,6 @@
+export default class NoteModel {
+    constructor(id, content) {
+        this.id = id;
+        this.content = content
+    }
+}
