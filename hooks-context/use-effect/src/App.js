f4eb056b8de41edc55160dde9050cd961b0557ba
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-25 14:36:24 +0300

    out id

--- hooks-context/use-effect/src/App.js
+++ hooks-context/use-effect/src/App.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect, Fragment} from 'react';
 import List from './List';
 import './App.css';
 import Details from './Details';
+import usePolling from './usePolling';
 
 export default function App() {
   const [info, setInfo] = useState({id: null})
+  const [{data: id}] = usePolling(process.env.REACT_APP_NAMES_URL)
 
   function handleChoise(id) {
     console.log(id)
