b91537eee8e2445a4c079a799ec714da038b5397
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-26 01:09:02 +0300

    deleted  replay Details

--- hooks-context/use-effect/src/App.js
+++ hooks-context/use-effect/src/App.js
@@ -6,26 +6,31 @@ import Details from './Details';
 export default function App() {
   const [info, setInfo] = useState({id: null})
   const [data, setData] = useState({id: null})
-
+ 
   function handleChoise(id) { // получение нового id
     setInfo({id})
   }
 
   useEffect(() => {
       if(info.id) {
-      fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id.id}.json`, {mode: 'cors'})
-      .then(response => {
-        if(response.ok) {
-          response.json()
-          .then(data => setData(prev => ({...prev, id: data}))) 
-      }})
-      .then(setInfo({id: 0}))
-      .catch(function(error) {
-        console.log(error);
-      })
-      setData({id: null}) // очистка Details
-    }
-  }, [info.id]);
+        fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id.id}.json`, {mode: 'cors'})
+        .then(response => {
+          if(response.ok) {
+            response.json()
+            .then(d => {
+              setData({id: d}) // setData(prev => ({...prev, id: d}))
+            })
+        }})
+        .then(setInfo({id: 0}))
+        .catch(function(error) {
+          console.log(error);
+        })
+        if((data.id !== null) && (data.id.id !==info.id.id)) {
+          setData({id: null})// очистка Details 
+        }
+        
+      }
+  }, [info.id, data]);
 
 
 
--- hooks-context/use-effect/src/Details.js
+++ hooks-context/use-effect/src/Details.js
@@ -2,15 +2,15 @@ import React, { Fragment, useState, useEffect } from 'react';
 import usePolling from './usePolling';
 
 export default function Details({data}) {
-    console.log(data)
-    //const [loading, setLoading] = useState(false)
-    
-  
-
+    //console.log(data)
     return (
         <Fragment>
             <div>
                 <img src={data.avatar} alt="avatar"/>
+                <h2>{data.name}</h2>
+                <p>City: {data.details.city}</p>
+                <p>Company: {data.details.company}</p>
+                <p>Position: {data.details.position}</p>
             </div>
         </Fragment>
     )
