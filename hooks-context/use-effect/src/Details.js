0d5312835142b177c0e4fae15c6f2cf7dfa4d6a2
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-25 17:38:43 +0300

    usePolling

--- hooks-context/use-effect/src/Details.js
+++ hooks-context/use-effect/src/Details.js
@@ -1,38 +1,18 @@
 import React, { Fragment, useState, useEffect } from 'react';
+import usePolling from './usePolling';
 
 export default function Details({info}) {
-    const [loading, setLoading] = useState(false)
     console.log(info)
+    const [loading, setLoading] = useState(false)
+    const data = usePolling(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/`, info.id)
+    console.log(data)
 
-    const getData = (info) => {
-        fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info}.json`, {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        })
-        .then(setLoading(true))
-        .then(response => {
-          if(response.ok) {
-            response.json()
-           /* .then(data => {
-              setData(data)
-              console.log(data)
-            })*/
-            .then(json =>  console.log(json))
-            .then(setLoading(false))
-          }
-        }) 
-        .catch(function(error) {
-          console.log(error);
-        })
-      }
+
 
     return (
         <Fragment>
             <div>
-                <img src={info.avatar} alt="avatar"/>
+                <img src={data.avatar} alt="avatar"/>
             </div>
         </Fragment>
     )
--- hooks-context/use-effect/src/usePolling.js
+++ hooks-context/use-effect/src/usePolling.js
@@ -1,8 +1,32 @@
-/*import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
-export default function usePolling(url) {
-    const [data, setData] = useState({id: null})
+export default function usePolling(url, id) {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(false);
 
+    useEffect(() => {
+        fetch(`${url}${id}.json`, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then(setLoading(true))
+        .then(response => {
+          if(response.ok) {
+            response.json()
+            .then(data => {
+              setData(data)
+              console.log(data)
+            })
+            .then(setLoading(false))
+          }
+        }) 
+        .catch(function(error) {
+          console.log(error);
+        })
+    }, [])
     
-    return ()
-}*/
+    return data
+}
