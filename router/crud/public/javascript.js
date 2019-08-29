ef4f7d2452d6f289d1563435507879265d165d0e
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-08-29 23:47:23 +0300

    HeadPage

---
+++ router/crud/public/javascript.js
@@ -0,0 +1,13 @@
+let textarea = document.querySelector('textarea')
+let form = document.querySelector('form')
+
+textarea.addEventListener('input', (event) => {
+    if(event.target.scrollTop > 0){
+        event.target.style.height = event.target.scrollHeight + "px";
+    }
+    if(event.target.value.length > 45) {
+        event.target.setAttribute('cols', 84)
+    } else {
+        event.target.setAttribute('cols', 20)
+    }
+  })
--- router/crud/src/HeadPage.js
+++ router/crud/src/HeadPage.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import useJsonFetch from './hooks/useJsonFetch';
 import Smewarik from './Smewarik.png'
+import like from './like.svg'
+import chat from './chat.svg'
+import smile from './smile.svg'
+import gif from './gif.svg'
+import photo from './photo.svg'
+import sticker from './sticker.svg'
+
 
 export default function HeadPage({url}) {
     const [data] = useJsonFetch(url, [])
@@ -9,26 +16,54 @@ export default function HeadPage({url}) {
     return (
         <div>
             <h1>Главная</h1>
-            <span>{data.map(o => <span key={o.id}>{o.content}</span>)}</span>
-            <div className="block">
-                <div className="create">
-                    <button>Создать пост</button>
-                </div>
-                <div className="content">
-                    <div className="head-content">
-                        <img src={Smewarik} alt="avatar" width={25}></img>
+            <span>{data.map(o => 
+                <span key={o.id}>  
+                    <div className="block">
+                        <div className="create">
+                            <button className="button-create">Создать пост</button>
+                        </div>
+                        <div className="content">
+                            <div className="head-content">
+                                <img className="avatar" src={Smewarik} alt="avatar" width={25}></img>
+                            </div>
+                            <div className="text-content">
+                                <h4>Ежик Колючкин</h4>
+                                <p>🙒 Основатель группы 4мин.</p>
+                            </div>
+                        </div>
+                        <div className="article">
+                            <p>{o.content}</p>
+                        </div>
+                        <div>
+                            <div className="footer">
+                                <button className="button-like" src={like}>
+                                    <img src={like} alt="like" width={15} className="like"/>
+                                    Нравится
+                                </button>
+                                <button className="button-commit">
+                                    <img src={chat} alt="chat" width={15} className="chat"/>
+                                    Комментировать
+                                </button>
+                            </div>
+                        </div>
+                        <div className="commit">
+                            <img className="avatar" src={Smewarik} alt="avatar" width={25}></img>
+                            <form method="post" name="commit">  
+                                <div className="input">
+                                    <textarea name="text" type="text" placeholder="Напишите комментарий" row="1"/>
+                                    <div className="img-reaction">
+                                        <img src={smile} alt="smile" width={15}></img>
+                                        <img src={photo} alt="addphoto" width={15}></img>
+                                        <img src={gif} alt="gif" width={15}></img>
+                                        <img src={sticker} alt="sticker" width={15}></img>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div className="text-content">
-                        <h4>Ежик Колючкин</h4>
-                        <p>🙒 Основатель группы 4мин.</p>
-                    </div>
-                </div>
-                <div className="article">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, mollitia sed! Sequi iure rem qui vel quos repellendus fugit minus molestiae ipsa quia eius, inventore eveniet, laboriosam dolores perferendis quisquam.</p>
-                </div>
-                
-                
-            </div>
+                </span>)}
+            </span>
         </div>
+        
     )
 }
